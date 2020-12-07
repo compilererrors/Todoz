6ed755bd7750a9653d6134af8dcd80da7d6c337a
Viktor <viktor.nyberg@hm.com>
2020-12-07 13:06:41 +0100

    added some styling and corrected the linking to stylesheet and scripts.js

--- src/main/java/com/example/Todoz/TodozController.java
+++ src/main/java/com/example/Todoz/TodozController.java
@@ -1,11 +1,19 @@
 package com.example.Todoz;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodozController {
 
-    @RequestBody
+    @GetMapping("/")
+    public String books(Model model) {
+
+
+        return "/index";
+    }
 
 }
