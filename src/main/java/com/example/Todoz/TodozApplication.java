ae8e40a9b6c88636fab8faa06b79a46ec27f1d43
Viktor <viktor.nyberg@hm.com>
2020-12-07 09:37:35 +0100

    Starter project

---
+++ src/main/java/com/example/Todoz/TodozApplication.java
@@ -0,0 +1,13 @@
+package com.example.Todoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodozApplication.class, args);
+	}
+
+}
